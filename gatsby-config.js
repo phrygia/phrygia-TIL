49a8d6cf4ee9440a2f65966ff6c8dd3fc6626af9
Lee Chaeyeon <dmsgp62@gmail.com>
2021-12-13 02:17:40 +0900

    css 추가 및 파일 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteMetadata: {
-    title: `Document by Code Bushi`,
-    name: `Code Bushi`,
-    siteUrl: `https://gatsby-theme-document.netlify.com`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
-    social: [
-      {
-        name: `github`,
-        url: `https://github.com/codebushi/gatsby-theme-document`
-      },
-      {
-        name: `twitter`,
-        url: `https://twitter.com/HuntaroSan`
-      }
-    ],
-    sidebarConfig: {
-      forcedNavOrder: ["/introduction", "/codeblock"],
-      ignoreIndex: true
-    }
-  },
-  plugins: [{ resolve: `gatsby-theme-document` }]
+    siteMetadata: {
+        title: `Today phrygia Learned`,
+        name: `Code Bushi`,
+        siteUrl: `https://phrygia.github.io/phrygia-TIL/`,
+        description: `This is my description that will be used in the meta tags and important for search results`,
+        social: [
+            {
+                name: `github`,
+                url: `https://github.com/phrygia`,
+            },
+            // {
+            //   name: `twitter`,
+            //   url: `https://twitter.com/HuntaroSan`
+            // }
+        ],
+        sidebarConfig: {
+            forcedNavOrder: ['/introduction', '/codeblock'],
+            ignoreIndex: true,
+        },
+    },
+    plugins: [{ resolve: `gatsby-theme-document` }],
 };
