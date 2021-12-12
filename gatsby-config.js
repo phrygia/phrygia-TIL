d648d8d515559645b7acfa22538d9f58a56b9448
Lee Chaeyeon <dmsgp62@gmail.com>
2021-12-13 03:00:02 +0900

    수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,20 @@
 module.exports = {
+    pathPrefix: `/phrygia-TIL`,
     siteMetadata: {
         title: `Today phrygia Learned`,
-        name: `Code Bushi`,
+        name: `phrygia`,
         siteUrl: `https://phrygia.github.io/phrygia-TIL/`,
-        description: `This is my description that will be used in the meta tags and important for search results`,
+        description: ``,
         social: [
             {
                 name: `github`,
                 url: `https://github.com/phrygia`,
             },
-            // {
-            //   name: `twitter`,
-            //   url: `https://twitter.com/HuntaroSan`
-            // }
         ],
         sidebarConfig: {
-            forcedNavOrder: ['/introduction', '/codeblock'],
+            forcedNavOrder: ['/introduction', '/monthly', '/codeblock'],
             ignoreIndex: true,
         },
     },
-    pathPrefix: `/phrygia-TIL`,
     plugins: [{ resolve: `gatsby-theme-document` }],
 };
