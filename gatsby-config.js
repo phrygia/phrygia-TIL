e7cbc49c821ab1767243cc204188ec67d4e71c1a
chaeyeonLee <dmsgp62@gmail.com>
2021-12-13 21:15:27 +0900

    다시 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    pathPrefix: `/phrygia-TIL`,
-    siteMetadata: {
-        title: `Today phrygia Learned`,
-        name: `phrygia`,
-        siteUrl: `https://phrygia.github.io/phrygia-TIL/`,
-        description: ``,
-        social: [
-            {
-                name: `github`,
-                url: `https://github.com/phrygia`,
-            },
-        ],
-        sidebarConfig: {
-            forcedNavOrder: ['/introduction', '/monthly', '/codeblock'],
-            ignoreIndex: true,
-        },
+  pathPrefix: `/phrygia-TIL`,
+  siteMetadata: {
+    title: `Today phrygia Learned`,
+    name: `phrygia`,
+    siteUrl: `https://phrygia.github.io/phrygia-TIL/`,
+    description: ``,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/phrygia`,
+      },
+    ],
+    sidebarConfig: {
+      forcedNavOrder: ["/introduction", "/monthly", "/codeblock"],
+      ignoreIndex: true,
     },
-    plugins: [{ resolve: `gatsby-theme-document` }],
+  },
+  plugins: [{ resolve: `gatsby-theme-document` }],
 };
