794b158c3ff7275f2c7ae51a139a2557ee53b5a3
chaeyeonLee <dmsgp62@gmail.com>
2021-12-15 21:43:18 +0900

    구글 애널리틱스 추적코드 추가 및 플러그인 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,14 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: `G-NMYSHMN3DT`, // 측정 ID
+        head: false, // head에 tracking script를 넣고 싶다면 true로 변경
+        anonymize: true,
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `content`,
