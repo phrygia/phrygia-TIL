5f0add8c5e3aa798724f1a39e7bfb2f6ab825d05
chaeyeonLee <dmsgp62@gmail.com>
2021-12-24 15:43:51 +0900

    애널리틱스 추적코드, 구글 search console 소유권확인 meta 태그 추

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,27 @@ module.exports = {
   description: "",
   head: [
     [
+      "meta",
+        { 
+          name: 'google-site-verification', 
+          content: 'Qwl1ByUT9JKdKH-gShcyEFtOESV8O62F4LH-zNIjiQw' 
+        },
+    ],
+    [
       "link",
       {
         rel: "stylesheet",
         href: `../styles/index.styl`,
       },
     ],
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-NMYSHMN3DT",
+      },
+    ],
+    ["script", {}, ["window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-NMYSHMN3DT');"]],
   ],
   plugins: [["sitemap", { hostname: "https://phrygia.github.io/phrygia-TIL/" }]],
   base: "/phrygia-TIL/",
